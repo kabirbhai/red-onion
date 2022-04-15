b2a09e0ada66a0e4da8aa1a7f171682a2241ae69
kabir <makkobir79@gmail.com>
2022-04-16 04:02:22 +0600

    json file add and firebase inisialize

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import Footer from "./Pages/Shared/Footer/Footer";
 import Header from "./Pages/Shared/Header/Header";
 import Home from "../src/Pages/Home/Home/Home";
---
+++ src/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA9gW5N1sWI-Fexr-Z1uE7sw8xjNSnA5oM",
+  authDomain: "red-onion-ca650.firebaseapp.com",
+  projectId: "red-onion-ca650",
+  storageBucket: "red-onion-ca650.appspot.com",
+  messagingSenderId: "653278260386",
+  appId: "1:653278260386:web:301789bdb3c2560746bbfb",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
