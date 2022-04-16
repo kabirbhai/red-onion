274227e96da1caa995cd7080e4625daee4313144
kabir <makkobir79@gmail.com>
2022-04-16 16:08:06 +0600

    login and sugn up section added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Footer from "./Pages/Shared/Footer/Footer";
 import Header from "./Pages/Shared/Header/Header";
 import Home from "../src/Pages/Home/Home/Home";
+import Login from "../src/Pages/Login/Login/Login";
+import Signup from "../src/Pages/Login/Signup/Signup";
 import NotFound from "./Pages/Shared/NotFound/NotFound";
 
 function App() {
@@ -13,6 +15,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
       <Footer />
---
+++ src/Pages/Login/Signup/Signup.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Signup = () => {
+  return (
+    <div>
+      <p>Please signup</p>
+    </div>
+  );
+};
+
+export default Signup;
