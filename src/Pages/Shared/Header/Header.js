f11f42fe981c8d6ea838b41044f36cf16f24a68a
kabir <makkobir79@gmail.com>
2022-04-16 14:11:40 +0600

    header complited

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,13 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Header.css";
 import Logo from "../../../images/logo.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 
 const Header = () => {
   return (
     <div>
       <nav>
-        <div className="logo">{Logo}</div>
-        <div>
+        <div className="logo">
+          <Link to="/">
+            {" "}
+            <img src={Logo} alt="Logo" />
+          </Link>
+        </div>
+        <div className="nav-item">
+          <Link to="/cart">
+            {" "}
+            <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon>
+          </Link>
           <Link to="/signup">Sign up</Link>
           <Link to="/login">Login</Link>
         </div>
--- src/firebase.init.js
+++ src/firebase.init.js
@@ -1,19 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA9gW5N1sWI-Fexr-Z1uE7sw8xjNSnA5oM",
-  authDomain: "red-onion-ca650.firebaseapp.com",
-  projectId: "red-onion-ca650",
-  storageBucket: "red-onion-ca650.appspot.com",
-  messagingSenderId: "653278260386",
-  appId: "1:653278260386:web:301789bdb3c2560746bbfb",
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
 const auth = getAuth(app);
 export default auth;
